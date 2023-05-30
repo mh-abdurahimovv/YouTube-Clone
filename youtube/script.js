f311edb7287b97dacee57988546ae99cba9aeb4f
Huseyn <abdurahimovm047@gmail.com>
2023-05-30 12:19:22 +0300

    fixation

--- youtube/script.js
+++ youtube/script.js
@@ -80,7 +80,7 @@
 
         };
 
-        getResources('http://localhost:3000/youtubeCards')
+        getResources('  http://localhost:4600/youtubeCards')
         .then(data => {
           data.forEach(({ videoLink, videoPicture, videoTime, profilePic, bloggerTittle, followers, videoTittle, videoStats }) => {
             new YoutubeCard(videoLink, videoPicture, videoTime, profilePic, bloggerTittle, followers, videoTittle, videoStats, '.everything .video-grid').render();
